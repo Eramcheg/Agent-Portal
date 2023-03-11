3306dd6d2f5c5a9b2af74af3dc40f03f997e4aaa
Eramcheg <eramcheg@gmail.com>
2023-03-11 20:44:59 +0100

    NEW: urls and views

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from users.views import Register, Profile
+from users.views import Register, Profile, Table
 from users.views import ShowTable
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', Register.as_view(),name='register'),
     path('Num1/', ShowTable.as_view(), name='table'),
+    path('table/', Table.as_view(), name='year'),
     path('Profile/', Profile.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,96 @@
+import ftplib
+import os.path
 import shutil
 import urllib.request
 from contextlib import closing
 
+import wget
 from django.contrib.auth import authenticate, login
 
 from django.shortcuts import render, redirect
 from django.views import View
-
+from openpyxl import load_workbook
 from users.forms import UserCreationForm
+
+
+class Table(View):
+    def get(self, request):
+
+        if request.user.is_authenticated:
+
+            username = str(request.user.username)
+            print(username)
+
+            name_html = username + "_" + str(request.GET.get('name'))
+            print(name_html)
+            trueOrFalse = self.create_table(name_html)
+            if trueOrFalse:
+                return render(request, name_html+'.html')
+            else:
+                return render(request, 'NotFoundHtml.html')
+        else:
+            return render(request, 'NotFoundHtml.html')
+    def create_table(self, name_html):
+        try:
+            link = f'ftp://admin:admin1234@192.168.56.1/{name_html}.xlsx'
+
+            with urllib.request.urlopen(link) as response, open(f"{name_html}.xlsx", 'wb') as out_file:
+                data = response.read()  # Read the contents of the file
+                out_file.write(data)  # Write the contents to the local file
+
+            wb = load_workbook(f'{name_html}.xlsx')
+            ws = wb['Sheet']
+            html = """<meta charset="utf-8">
+             <table border="1" class="dataframe">
+                         <thead>
+                             <tr style="text-align: right;">\n"""
+            k = 0
+
+            for i in ws.iter_rows():
+                if k > 0:
+                    html += '<tr>\n'
+                for j in range(len(i)):
+                    value = str(i[j].value)
+                    color = str(i[j].fill.start_color.rgb)
+                    if (value == 'None'):
+                        value = ' '
+                    if color == '00000000':
+                        color = 'FFFFFFFF'
+
+                    if k < 1:
+                        html += "<th style='background-color: #" + color[2:8] + "'>" + value + "</th>\n"
+                    else:
+                        html += "<td style='background-color: #" + color[2:8] + "'>" + value + "</td>\n"
+                html += '</tr>\n'
+                if k < 1:
+                    html += """</thead>
+              <tbody>"""
+                k += 1
+            html += """ </tbody>
+            </table>"""
+            html.encode('UTF-8')
+            with open('templates/'+name_html+".html", 'w', encoding='utf-8') as f:
+                f.write(html)
+                return True
+        except :
+            return False
+
 
 
 class ShowTable(View):
     template_name = 'Cabine.html'
-    # with closing(urllib.request.urlopen('ftp://admin:admin1234@192.168.56.1/')) as r:
-    #     with open('OutputResult249.xlsx', 'wb') as f:
-    #         shutil.copyfileobj(r, f)
-    link = 'ftp://admin:admin1234@192.168.56.1/OutputResult249.xlsx'
+
 
     def get(self, request):
+        if request.user.is_authenticated:
+
+            username = str(request.user.username)
+            print(username)
+        print(str(request.GET.get('name')))
+
         return render(request, self.template_name)
+
+
 class Profile(View):
     template_name = 'Profile.html'
     def get(self, request):
