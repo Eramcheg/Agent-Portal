516dbc2e85bfba9ead6d219b8c3b8b0e4aef2432
Eramcheg <eramcheg@gmail.com>
2023-03-16 13:30:44 +0100

    Design features

--- Agent_Portal_Authorization/settings.py
+++ Agent_Portal_Authorization/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Agent_Portal_Authorization.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'users/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
